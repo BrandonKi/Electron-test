0e8b1b6d505f98c0f71fb8d0513d86e87b6fce26
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-01-07 18:58:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const fs = require('fs');
+const dialog = require('electron').remote;
 
-const {app, BrowserWindow, Menu, ipcMain} = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 let addWindow;
 
 //Listen for the app to be ready
 
-app.on( 'ready', function(){
+app.on('ready', function () {
     //create new window
     mainWindow = new BrowserWindow(
         {
-            webPreferences: {nodeIntegration: true} 
+            webPreferences: { nodeIntegration: true }
         }
     );
     //load html file into the window
@@ -23,11 +25,11 @@ app.on( 'ready', function(){
         slashes: true
     }));
     //Quit app when closed
-    mainWindow.on('closed', function(){
+    mainWindow.on('closed', function () {
         app.quit()
     });
 
-    mainWindow.on('resize', function(){
+    mainWindow.on('resize', function () {
         mainWindow.webContents.send('get:canvas');
     });
 
@@ -38,13 +40,13 @@ app.on( 'ready', function(){
 });
 
 // add window 
-function createAddWindow(){
+function createAddWindow() {
     //create new window
     addWindow = new BrowserWindow({
         width: 300,
         height: 200,
         title: 'Add Shoppinbg List Item',
-        webPreferences: {nodeIntegration: true} 
+        webPreferences: { nodeIntegration: true }
     });
     //load html file into the window
     addWindow.loadURL(url.format({
@@ -53,18 +55,18 @@ function createAddWindow(){
         slashes: true
     }));
     //garbage collection/optimization
-    addWindow.on('closed', function(){
-        addWindow= null;
+    addWindow.on('closed', function () {
+        addWindow = null;
     })
 }
 
 //catch item:add
-ipcMain.on('item:add', function(e, item){
+ipcMain.on('item:add', function (e, item) {
     mainWindow.webContents.send('item:add', item);
     addWindow.close();
 })
 
-ipcMain.on('canvas:resize', function(e, canvas){
+ipcMain.on('canvas:resize', function (e, canvas) {
     canvas.width = mainWindow.innerWidth;
     canvas.height = mainWindow.innerHeight;
 })
@@ -76,14 +78,24 @@ const mainMenuTemplate = [
         label: 'File',
         submenu: [
             {
-                label: 'Add Item',
+                label: 'Open File',
                 click(){
+                    dialog.showOpenDialog({properties: ['openFile', 'multiSelections']}, function (files) {
+                        if (files !== undefined) {
+                            console.log(files);
+                        }
+                    });
+                }
+            },
+            {
+                label: 'Add Item',
+                click() {
                     createAddWindow()
                 }
             },
             {
                 label: 'clear items',
-                click(){
+                click() {
                     mainWindow.webContents.send('item:clear');
                 }
             },
@@ -92,7 +104,7 @@ const mainMenuTemplate = [
 
                 accelerator: process.platform == 'win32' ? 'Command+Q' : 'Ctrl+Q',   // accelerator is used for key events
 
-                click(){             // add click event
+                click() {             // add click event
                     app.quit();
                 }
             }
@@ -101,16 +113,18 @@ const mainMenuTemplate = [
 ];
 
 //if mac then add an empty object to menu
-if(process.platform == 'darwin')
+if (process.platform === 'darwin')
     mainMenuTemplate.unshift({}); // insert an element at the begining of the array
 
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
     mainMenuTemplate.push({
         label: 'Dev Tools',
         submenu: [
             {
                 label: 'Toggle',
-                click(item, focusedWindow){
+
+                accelerator: "Ctrl+I",
+                click(item, focusedWindow) {
                     focusedWindow.toggleDevTools();
                 }
             },
