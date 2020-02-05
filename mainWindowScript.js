2061d3f27e5b8093e94458c2fc94e8786f916834
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-02-04 19:32:47 -0500

    Can now save files

--- mainWindowScript.js
+++ mainWindowScript.js
@@ -1,23 +1,29 @@
 const electron = require('electron');
 const { ipcRenderer } = electron;
 
-var content = document.getElementById('content');
-var lineNums = document.getElementById('lineNums');
-var text = document.getElementById('text-container');
-var lastLine;
-var currentLineNum;
-var currentLines;
-var filepath;
-var dropdownIsVisible = false;
+const content = document.getElementById('content');
+const lineNums = document.getElementById('lineNums');
+const text = document.getElementById('text-container');
+let lastLine;
+let currentLineNum;
+let currentLines;
+let filepath;
+let dropdownIsVisible = false;
 const fs = require('fs');
+const titleText = document.getElementById('title-text');
 
-const data = require(process.cwd() + '\\.user-settings\\data.json');
-console.log(data);
+let data;
+const app_path = electron.remote.app.getPath('userData');
+try{
+    data = require(app_path + '\\data.json');
+}catch(err){
+    data = undefined;
+}
 // const spawn = require('child_process').spawn;
 // const bat = spawn('cmd.exe', ['/c', 'C:\\Users\\Kirin\\OneDrive\\Desktop\\Electron-test\\test.bat', '1111', '3333333']);  IT WORKS!!!!!
 
 // bat.stdout.on('data', (data) => {
-//     var str = String.fromCharCode.apply(null, data);          output from batch also option 1 for a callback
+//     let str = String.fromCharCode.apply(null, data);          output from batch also option 1 for a callback
 //     console.info(str);
 // });
 
@@ -25,33 +31,68 @@ console.log(data);
 //      console.log(`Child exited with code ${code}`);      output from batch also option 2 for a callback     
 // });
 
+if(data == undefined || data.cwd == undefined){
+    initWithDirectory(null);
+}
+else{
+    initWithDirectory(data.cwd);
+}
+
+function initWithDirectory(dir){
+    if(dir == undefined){
+        document.getElementById('lineNums').innerHTML = '';
+        document.getElementById('text-container').innerHTML = '';
+        const lines = [0 , 0, 0];
+        const span = document.createElement('SPAN');
+        span.style = 'display:block;position:relative;left:50px;background-color:#333B42;color:white;word-break:keep-all;white-space:nowrap';
+        span.id = 'textSpan';
+        span.innerHTML = "\n\n";
+        span.innerHTML += '\n';
+        span.contentEditable = 'plaintext-only';
+        const numOfLines = lines.length + 1;  
+        lines = lines.toString().replace(/,/g, ''); // replace all commas with nothing
+        text.appendChild(span);
+        for (lastLine = 1; lastLine < numOfLines; lastLine++) {
+            const numSpan = document.createElement('SPAN');
+            numSpan.style = 'position:absolute;width:25px;height:22px;user-select:none;padding:2px;margin:0;z-index:-3;color:#AAAAAA;font-size:12px;text-align:right;';
+            numSpan.innerHTML = lastLine;
+            lineNums.appendChild(numSpan);
+            lineNums.appendChild(document.createElement('BR'));
+        }
+    }
+    else{
+        titleText.innerHTML = dir.substring(dir.lastIndexOf('\\')+1);
+    }
+}
+
 content.addEventListener('scroll', (e) => {
     content.scrollLeft = 0;
-    var topPos = content.scrollTop;
+    const topPos = content.scrollTop;
     lineNums.scrollTop = topPos;
 });
 
 document.addEventListener('keydown', (e) => {
-    if (e.code == "Enter" || (e.code == "KeyZ" && e.ctrlKey)) {
-    console.log(document.activeElement);
+    if (e.code === "Enter" || (e.code === "KeyZ" && e.ctrlKey)) {
         if (document.activeElement == document.getElementById('textSpan')) {
-            let span = document.createElement('SPAN');
-            span.style = 'position:absolute;width:25px;height:22px;user-select:none;padding:2px;margin: 0;z-index:-3;color:#AAAAAA;font-size:12px;text-align:right;';
+            const span = document.createElement('SPAN');
+            span.style = 'position:absolute;width:25px;height:22px;user-select:none;padding:2px;margin:0;z-index:-3;color:#AAAAAA;font-size:12px;text-align:right;';
             span.innerHTML = lastLine++;
             lineNums.appendChild(span);
             lineNums.appendChild(document.createElement('BR'));
         }
     }
+    if(e.code === "KeyS" && e.ctrlKey)
+        saveCurrentFile();
 });
 
 document.addEventListener('keyup', (e) => {
-    if (e.code === "Backspace" || (e.code === "KeyZ" && e.ctrlKey && e.shiftKey)) {
-        var arr = text.children[0].innerHTML.split('\n');
-        if (arr[arr.length - 1] == "") {
+    if (e.code === "Backspace" || (e.code === "KeyZ" && e.ctrlKey)) {
+        const arr = text.children[0].innerHTML.split('\n');
+        if (arr[arr.length - 1] === "") {
             arr.pop();
         }
         tempstr = text.children[0].innerHTML;
-        var temp = Array.from(lineNums.children);
+        const temp = Array.from(lineNums.children);
         while (arr.length < temp.length / 2) {
             temp[temp.length - 1].remove();
             temp[temp.length - 2].remove();
@@ -63,13 +104,13 @@ document.addEventListener('keyup', (e) => {
 });
 
 ipcRenderer.on('save-last-closed-file', function (e) {
-    filepath = (filepath == undefined || filepath == "" ? data.lastfilepath : filepath);
+    filepath = (filepath === undefined || filepath === "" ? data.lastfilepath : filepath);
     const data = {
         lastfilepath: filepath,
         cwd: process.cwd()
     }
     const jsonString = JSON.stringify(data)
-    fs.writeFile(process.cwd() + '\\.user-settings\\data.json', jsonString, err => {
+    fs.writeFile(app_path + '\\data.json', jsonString, err => {
         if (err) {
             console.log('Error writing file', err)
         } else {
@@ -80,9 +121,9 @@ ipcRenderer.on('save-last-closed-file', function (e) {
 
 
 document.getElementById('file').addEventListener('click', function () {
-    var temp = document.getElementById('file-dropdown');
+    const temp = document.getElementById('file-dropdown');
     hideAllDropdowns('file');
-    if (temp.style.display == "block") {
+    if (temp.style.display === "block") {
         temp.style.display = "none";
         dropdownIsVisible = false;
     }
@@ -99,7 +140,7 @@ document.getElementById('open-file').addEventListener('click', function (event) 
     }).then(result => {
         openFile(result.filePaths);
     }).catch(function(err){
-        //do nothing because that sounds like a good idea
+        console.log(err);
     });
     hideAllDropdowns();
 });
@@ -110,24 +151,25 @@ document.getElementById('open-last-file').addEventListener('click', function (ev
 });
 
 function openFile(path) {
+    console.log(path);
     filepath = path;
     document.getElementById('lineNums').innerHTML = '';
     document.getElementById('text-container').innerHTML = '';
-    fs.readFile(path[0].substring(path[0].lastIndexOf('\\') + 1), (err, data) => {
+    fs.readFile(path[0], (err, file_data) => {
         if (err) throw err;
-        let lines = data.toString().split('\n');
-        let span = document.createElement('SPAN');
-        span.style = 'display:block;position:relative;left:50px;background-color:#333B42;color: white;';
+        let lines = file_data.toString().split('\n');
+        const span = document.createElement('SPAN');
+        span.style = 'display:block;position:relative;left:50px;background-color:#333B42;color:white;word-break:keep-all;white-space:nowrap';
         span.id = 'textSpan';
-        span.innerHTML = data.toString();
+        span.innerHTML = file_data.toString();
         span.innerHTML += '\n';
         span.contentEditable = 'plaintext-only';
-        let numOfLines = lines.length + 1;  
+        const numOfLines = lines.length + 1;  
         lines = lines.toString().replace(/,/g, '');
         text.appendChild(span);
         for (lastLine = 1; lastLine < numOfLines; lastLine++) {
-            let numSpan = document.createElement('SPAN');
-            numSpan.style = 'position:absolute;width:25px;height:22px;user-select:none;padding:2px;margin: 0;z-index:-3;color:#AAAAAA;font-size:12px;text-align:right;';
+            const numSpan = document.createElement('SPAN');
+            numSpan.style = 'position:absolute;width:25px;height:22px;user-select:none;padding:2px;margin:0;z-index:-3;color:#AAAAAA;font-size:12px;text-align:right;';
             numSpan.innerHTML = lastLine;
             lineNums.appendChild(numSpan);
             lineNums.appendChild(document.createElement('BR'));
@@ -135,10 +177,24 @@ function openFile(path) {
     });
 }
 
+document.getElementById('save-file').addEventListener('click', function(){
+    saveCurrentFile();
+});
+
+function saveCurrentFile(){
+    const temp = document.getElementById('textSpan').innerHTML;
+    fs.writeFile(filepath[0], temp.substring(0, temp.lastIndexOf('\n')), function(err) {
+        if(err)
+            console.log(err);
+    });
+}
+
+
+
 document.getElementById('edit').addEventListener('click', function () {
-    var temp = document.getElementById('edit-dropdown');
+    const temp = document.getElementById('edit-dropdown');
     hideAllDropdowns('edit');
-    if (temp.style.display == "block") {
+    if (temp.style.display === "block") {
         temp.style.display = "none";
         dropdownIsVisible = false;
     }
@@ -149,9 +205,9 @@ document.getElementById('edit').addEventListener('click', function () {
 });
 
 document.getElementById('view').addEventListener('click', function () {
-    var temp = document.getElementById('view-dropdown');
+    const temp = document.getElementById('view-dropdown');
     hideAllDropdowns('view');
-    if (temp.style.display == "block") {
+    if (temp.style.display === "block") {
         temp.style.display = "none";
         dropdownIsVisible = false;
     }
@@ -162,9 +218,9 @@ document.getElementById('view').addEventListener('click', function () {
 });
 
 document.getElementById('help').addEventListener('click', function () {
-    var temp = document.getElementById('help-dropdown');
+    const temp = document.getElementById('help-dropdown');
     hideAllDropdowns('help');
-    if (temp.style.display == "block") {
+    if (temp.style.display === "block") {
         temp.style.display = "none";
         dropdownIsVisible = false;
     }
@@ -210,7 +266,8 @@ document.getElementById('run').addEventListener('mouseover', function () {
 });
 
 
-document.getElementById('content').addEventListener('mousedown', function () {
+document.getElementById('content').addEventListener('mousedown', function (e) {
+    document.getElementById('cursor-follower').style.top = "" + (e.clientY - 60) + "px";
     hideAllDropdowns();
     dropdownIsVisible = false;
 });
@@ -218,31 +275,31 @@ document.getElementById('content').addEventListener('mousedown', function () {
 
 
 function hideAllDropdowns(str) {
-    if (str == 'file') {
+    if (str === 'file') {
         document.getElementById('edit-dropdown').style.display = "none";
         document.getElementById('view-dropdown').style.display = "none";
         document.getElementById('help-dropdown').style.display = "none";
         document.getElementById('run-dropdown').style.display = "none";
     }
-    else if (str == 'edit') {
+    else if (str === 'edit') {
         document.getElementById('file-dropdown').style.display = "none";
         document.getElementById('view-dropdown').style.display = "none";
         document.getElementById('help-dropdown').style.display = "none";
         document.getElementById('run-dropdown').style.display = "none";
     }
-    else if (str == 'view') {
+    else if (str === 'view') {
         document.getElementById('file-dropdown').style.display = "none";
         document.getElementById('edit-dropdown').style.display = "none";
         document.getElementById('help-dropdown').style.display = "none";
         document.getElementById('run-dropdown').style.display = "none";
     }
-    else if (str == 'help') {
+    else if (str === 'help') {
         document.getElementById('file-dropdown').style.display = "none";
         document.getElementById('edit-dropdown').style.display = "none";
         document.getElementById('view-dropdown').style.display = "none";
         document.getElementById('run-dropdown').style.display = "none";
     }
-    else if (str == 'run'){
+    else if (str === 'run'){
         document.getElementById('file-dropdown').style.display = "none";
         document.getElementById('edit-dropdown').style.display = "none";
         document.getElementById('view-dropdown').style.display = "none";
